b4f251d0e9012e59509e14fb456b7b355980a403
Thanh Dat <datmathoingu1100@gmail.com>
2020-09-30 17:21:17 +0700

    Initial commit

---
+++ Generic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+     class Generic<T>
+    {
+        public T a { get; set; }
+        public T b { get; set; }
+        public void Compare<T>(T a, T b)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            if (a.Equals(b))
+            {
+
+                Console.WriteLine($"Hai giá trị bằng nhau ({a} = {b})");
+            }
+            else
+            {
+
+                Console.WriteLine($"Hai giá trị không bằng nhau ({a} != {b})");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> generic = new Generic<int>();
+            generic.Compare<int>(2, 2);
+            generic.Compare<string>("abc", "abc");
+
+        }
+    }
+
+}
